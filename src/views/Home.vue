5439967e2890b1cc4870f07d90c0e72075b734ce
iamdjoky <maude2413@hotmail.fr>
2021-12-09 14:43:29 +0100

    boutique_goodies

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,16 @@
 <template>
-    <div class="section">
-        <Sample title="Home" content="Ceci est la description !" :items="DataSample.items" />
-        <ImgContent :src="require('../assets/img/logo.png')" title="Image + contenu" content="Ceci est la description !" />
+    <div>
     </div>
 </template>
 
 <script>
-    // Import dynamic data
-    import { DataSample } from '../data/sample.js'
-
-    // Import component
-    import Sample from '../components/Sample.vue'
-    import ImgContent from '../components/ImgContent.vue'
-
-    console.log(DataSample.items)
+    
 
     export default {
         // Define dynamic data to pass in the template
-        created() {
-            this.DataSample = DataSample
-        },
+        
         name: 'Home',
         // Specify which components are used
-        components: {
-            ImgContent,
-            Sample
-        }
+        
     }
 </script>
